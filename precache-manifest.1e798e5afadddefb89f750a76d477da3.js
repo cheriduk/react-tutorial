5f0d8664fd483b9d2c862ee6c126484b40cf556b
e <wb-dk780530@alibaba-inc.com>
2020-08-20 20:03:49 +0800

    Updates

---
+++ precache-manifest.1e798e5afadddefb89f750a76d477da3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "011565ff554c0dfd5045",
+    "url": "/react-tutorial/static/css/main.9fe1094c.chunk.css"
+  },
+  {
+    "revision": "011565ff554c0dfd5045",
+    "url": "/react-tutorial/static/js/main.011565ff.chunk.js"
+  },
+  {
+    "revision": "f3b84ad1a4a60d245526",
+    "url": "/react-tutorial/static/js/1.f3b84ad1.chunk.js"
+  },
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "1d76affbadc1d4f2533892aecb48cf03",
+    "url": "/react-tutorial/index.html"
+  }
+];
